fe7af688982f8469aac9b6b5d4e6a0167af5d420
shashank shrivastava <internstudent02@gmail.com>
2024-12-31 19:45:42 +0530

    SWIGGY Api

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -6,3 +6,5 @@ export const LOGO_URL =
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+export const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
