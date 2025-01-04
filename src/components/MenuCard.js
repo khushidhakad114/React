784adf2c06bd68c4e26e1b96669739f7a3a4ee2f
Khushi <khushidhakad2003@gmail.com>
2025-01-04 21:04:55 +0530

    menucard

---
+++ src/components/MenuCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuCard = () => {
+  return <div></div>;
+};
+
+export default MenuCard;
--- src/components/MenuRes.js
+++ src/components/MenuRes.js
@@ -10,10 +10,7 @@ const MenuRes = () => {
   }, [id]);
   const getMenuId = async () => {
     const data = await axios.get(MENU_API + id);
-    console.log(
-      data.data.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card
-        .card.categories[1].itemCards[1].card.info.name
-    );
+    console.log(data?.data?.data);
   };
   return <div>hi i am menu {id}</div>;
 };
