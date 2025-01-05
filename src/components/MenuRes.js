d746c4e05de78ec5deac9537067966e64c9c8b8f
Khushi <khushidhakad2003@gmail.com>
2025-01-05 17:38:42 +0530

    categories

--- src/components/MenuRes.js
+++ src/components/MenuRes.js
@@ -2,15 +2,22 @@ import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "./utils/constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const MenuRes = () => {
+  const [menu, setMenu] = useState("");
   const { id } = useParams();
   useEffect(() => {
     getMenuId();
   }, [id]);
   const getMenuId = async () => {
     const data = await axios.get(MENU_API + id);
-    console.log(data?.data?.data);
+    const categories =
+      data?.data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    const filteredCategories = categories.filter((c) => {
+      ("@type");
+    });
+    console.log(filteredCategories);
+    setMenu(filteredCategories);
   };
   return <div>hi i am menu {id}</div>;
 };
