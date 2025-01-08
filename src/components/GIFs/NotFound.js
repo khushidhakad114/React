d1d7afb4a940ebd86a2b2e927c246b677d84a75a
Khushi <khushidhakad2003@gmail.com>
2025-01-08 20:56:50 +0530

    not found

--- src/components/GIFs/NotFound.js
+++ src/components/GIFs/NotFound.js
@@ -3,7 +3,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center absolute left-0 right-0 top-0 bottom-0">
+    <div className="flex justify-center">
       <DotLottieReact
         src="https://lottie.host/3168d039-8970-4560-88a9-fc6dfbc62eb3/KUv53FV2mN.lottie"
         loop
